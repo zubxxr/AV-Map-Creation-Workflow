cddea47f46fcd9620b1b00df8357c786bae64b2c
Zubair <91695367+zubxxr@users.noreply.github.com>
2025-02-14 02:08:14 -0500

    Update process.sh

--- process.sh
+++ process.sh
@@ -4,6 +4,7 @@
 OSM_FILE="/app/map.osm"
 OBJ_FILE="/app/3D_Model/output.obj"
 PCD_FILE="/app/output.pcd"
+MTL_FILE="/app/3D_Model/output.obj.mtl"
 FIRST_TRANSFORM="/app/first_transformation.pcd"
 SECOND_TRANSFORM="/app/transformed_top_down_view.pcd"
 FINAL_PCD="/app/pointcloud_map.pcd"
@@ -16,17 +17,34 @@ if [ ! -f $FINAL_PCD ]; then
     echo "Created empty $FINAL_PCD"
 fi
 
+# Check if the output.pcd file exists
+if [ ! -f $PCD_FILE ]; then
+    touch $PCD_FILE
+    echo "Created empty $PCD_FILE"
+fi
+
 # Run osm2world to generate OBJ
 echo "Generating OBJ file..."
 java -jar /app/osm2world.jar --input $OSM_FILE -o $OBJ_FILE
 
 echo "OBJ File created successfully!"
 
-# Run pcl_mesh_sampling to generate PCD
-echo "Generating PCD file..."
-pcl_mesh_sampling $OBJ_FILE $PCD_FILE -leaf_size 0.5 -no_vis_result -n_samples 1000000
+# Clean the MTL file
+if [ -f "$MTL_FILE" ]; then
+    echo "Cleaning MTL file..."
+    python3 /app/clean_mtl_file.py "$MTL_FILE"
+    echo "MTL File cleaned successfully!"
+else
+    echo "Warning: MTL file not found. Skipping cleaning step."
+fi
 
+# Run CloudCompare to generate PCD
+echo "Generating PCD file..."
+CloudCompare -SILENT -O $OBJ_FILE -AUTO_SAVE OFF -SAMPLE_MESH DENSITY 100 -C_EXPORT_FMT PCD -SAVE_CLOUDS
 echo "Sampled PCD File created successfully!"
+
+# Ensure the output file is moved to the correct location
+mv /app/3D_Model/output_SAMPLED_*.pcd $PCD_FILE
 
 # Apply transformations
 echo "Applying first transformation..."
